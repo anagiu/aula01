cc41c166bfaf9294c5c363758906a1f1850616cf
anagiu <126169431+anagiu@users.noreply.github.com>
2023-02-24 09:58:52 -0300

    remoção de uma vírgula

--- produtos.php
+++ produtos.php
@@ -5,5 +5,5 @@ $loader = new \Twig\Loader\Filesystemloader('./templates');
 $twig = new \Twig\Environment ($loader);
 $template = $twig ->load('produtos.html');
 echo $template->render([
-    'titulo' => 'Produtos',
+    'titulo' => 'Produtos'
 ]);
